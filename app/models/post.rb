8a5b5bfcf8d6477dcbd9c7fddc49716e6a361781
AnaisR7 <anais-r7@hotmail.fr>
2018-02-02 22:46:44 +0100

    init commit

---
+++ app/models/post.rb
@@ -0,0 +1,10 @@
+class Post < ActiveRecord::Base
+  validates :vignette, presence: true
+  validates :titre, presence: true
+  validates :date, presence: true
+  validates :chapeau, presence: true
+  validates :nomAuteur, presence: true
+
+  mount_uploader :vignette, VignetteUploader
+
+end
